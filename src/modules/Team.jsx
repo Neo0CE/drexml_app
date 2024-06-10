8dd5070a1dc1972edf9068232be8238211ea8db3
Neo0CE <jgomezrz98@gmail.com>
2024-06-10 10:41:45 +0200

    Commit 10-06-2024

--- src/modules/Team.jsx
+++ src/modules/Team.jsx
@@ -118,22 +118,7 @@ const navigation = [
                 </a>
             </div>
 
-            <div className='seven'>
-                <a href="https://www.linkedin.com/in/javier-g%C3%B3mez-rodr%C3%ADguez-3891871a2/">
-                    <img className="image-people" src="https://media.licdn.com/dms/image/C5603AQHrgJtU4NZnmQ/profile-displayphoto-shrink_200_200/0/1625054856018?e=2147483647&v=beta&t=rDRaSgRJ2ErwD0dpJKR-R8YWeFJdpULlz7pmugRz-Mg" 
-                    alt="Javier Gómez Rodríguez"/>
-                    <div>
-                        <section className='head_name'>
-                        <span>Javier Gómez Rodríguez</span>
-                        <p>Master Student</p>
-                    </section>
-
-                    <section>
-                        <span className='team_content'>Physicist | Data Scientist.</span>
-                    </section>
-                    </div>
-                </a>
-            </div>
+           
 
            
 
